783c151f702b66db33b2840c5a1eccc3b88b2a98
Mariia Palyga <kiryakovam@gmail.comgit  config --global user.name  >
2020-01-11 20:06:17 +0400

    third commit into develop

--- app/src/main/java/com/example/service/MainActivity.java
+++ app/src/main/java/com/example/service/MainActivity.java
@@ -2,27 +2,66 @@ package com.example.service;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.IBinder;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    SortService sortService;
+    boolean bound = false;
     private Button sortButton;
+    private TextView info;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        info = findViewById(R.id.app_name);
+
         sortButton = findViewById(R.id.sort_button);
         sortButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startService(
-                        new Intent(MainActivity.this, SortService.class));
+                Intent intent = new Intent(MainActivity.this, SortService.class);
+                bindService(intent, connection, Context.BIND_AUTO_CREATE);
+                if (bound) {
+                    info.setText(Arrays.toString(sortService.sortArray()));
+                }
             }
         });
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (bound) {
+            unbindService(connection);
+            bound = false;
+        }
+    }
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            SortService.LocalBinder binder = (SortService.LocalBinder) service;
+            sortService = binder.getService();
+            bound = true;
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            bound = false;
+        }
+    };
 }
--- app/src/main/java/com/example/service/SortService.java
+++ app/src/main/java/com/example/service/SortService.java
@@ -1,44 +1,36 @@
 package com.example.service;
 
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
-import android.util.Log;
-import android.widget.Toast;
-
-import androidx.annotation.Nullable;
 
 import java.util.Arrays;
 import java.util.Random;
 
 public class SortService extends Service {
+    private int[] array;
+    private final IBinder binder = new LocalBinder();
 
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+
+    public class LocalBinder extends Binder {
+        SortService getService() {
+            return SortService.this;
+        }
     }
 
-    public void sortArray (){
-        int[] array = new int[100];
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+
+    public int[] sortArray() {
+        array = new int[100];
         for (int i = 0; i < array.length; i++) {
             array[i] = new Random().nextInt(10);
         }
         Arrays.sort(array);
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        sortArray();
-        stopSelf();
-        return START_STICKY;
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(this, "Array Sorted",
-                Toast.LENGTH_SHORT).show();
+        return array;
     }
 }
